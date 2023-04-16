bd93ad8d11549d4b6febf2b3af4285d31c8ea439
saitamsaitam <matiaslindblom157@gmail.com>
2023-04-16 21:11:51 +0300

    deleting directory

--- Tehtävä_09 Mock-kirjaston käyttöä (Eclipse, JUnit 5, Mockito)/mavenproject/src/main/java/com/simplilearn/mavenproject/IHinnoittelija.java
+++
@@ -1,9 +0,0 @@
-package com.simplilearn.mavenproject;
-
-
-public interface IHinnoittelija {
-	public abstract float getAlennusProsentti(Asiakas asiakas, Tuote tuote);
-	public abstract void aloita();
-	public abstract float setAlennusProsentti(Asiakas asiakas, double prosentti);
-	public abstract void lopeta();
-}
